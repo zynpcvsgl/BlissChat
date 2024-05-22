8801f43f6dfe19c7ad7794090c06ec19c63c7740
ZEYNEP ÇAVUŞOĞLU <94304314+zynpcvsgl@users.noreply.github.com>
2024-05-22 22:40:35 +0300

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,18 @@
-import bcrypt from "bcrypt";
-import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import GithubProvider from "next-auth/providers/github";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import authOptions  from "../../../lib/auth";
 
-import prisma from "@/app/libs/prismadb";
+const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  pages: {
+    signIn: "/signin",
+  },
+};
 
 const handler = NextAuth(authOptions);
 
-
-
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
